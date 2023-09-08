b0abbe37efc0325cd2ba194537727785f480a859
Ali <aliifarajj@hotmail.com>
2023-09-08 18:06:21 +0300

    signup and getAllUsers endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 import express from "express";
+import mongoose from "mongoose"; 
+import router from "./routes/user-routes";
+
 const port = 4000;
 const app = express();
 
-app.use("/home", (req, res) => {
-    res.send("hello world")
-})
+app.use(express.json())
 
-app.listen(port, () => {
+app.use("/api/user", router)
+
+
+mongoose.connect("mongodb+srv://ali:DuW8ByeaV8xZ967n@cluster0.qcyx9mh.mongodb.net/Blog?retryWrites=true&w=majority")
+.then(() => app.listen(port, () => {
     console.log("Live on port " + port)
-})
+})).catch((err) => console.log(err))
