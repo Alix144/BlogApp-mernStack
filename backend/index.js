2a5629b750ce08bd6f6e583855e0c4ac56f46cb3
Ali <aliifarajj@hotmail.com>
2023-09-09 02:39:34 +0300

    add crud operations endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose"; 
 import router from "./routes/user-routes.js";
+import blogRouter from "./routes/blogs-routes.js";
 
 const port = 4000;
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json())
 
 app.use("/api/user", router)
-
+app.use("/api/blog", blogRouter)
 
 mongoose.connect("mongodb+srv://ali:DuW8ByeaV8xZ967n@cluster0.qcyx9mh.mongodb.net/Blog?retryWrites=true&w=majority")
 .then(() => app.listen(port, () => {
