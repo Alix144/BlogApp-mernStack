fc9393c86a24d5799501a32348b88608b75f03e3
Ali <aliifarajj@hotmail.com>
2023-09-13 19:58:13 +0300

    small changes

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 import router from "./routes/user-routes.js";
 import blogRouter from "./routes/blogs-routes.js";
 import cors from 'cors';
+import { config } from 'dotenv';
+config();
 
-const port = 4000;
+const port = process.env.port;
 const app = express();
 
 app.use(cors())
@@ -13,7 +15,7 @@ app.use(express.json())
 app.use("/api/user", router)
 app.use("/api/blog", blogRouter)
 
-mongoose.connect("mongodb+srv://ali:DuW8ByeaV8xZ967n@cluster0.qcyx9mh.mongodb.net/Blog?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => app.listen(port, () => {
     console.log("Live on port " + port)
 })).catch((err) => console.log(err))
